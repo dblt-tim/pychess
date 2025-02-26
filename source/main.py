d7ad79a995b117ba57518d5c2acbcd4afda3a2e4
Fulgrim2 <leoeudeline50@gmail.com>
2025-02-26 09:41:22 +0100

    modification  de la classe Game pour ajouter les deux joueurs

--- source/main.py
+++ source/main.py
@@ -2,6 +2,8 @@
 PyChess 
 
 """
+import random 
+import time 
 def init_grid(): 
     """défini le plateau et setup les pions"""
     grid=[["" for i in range(8)]for j in range(8)]
@@ -11,8 +13,18 @@ def init_grid():
     grid[7]=["wR","wN","wB","wK","wQ","wB","wN","wR"]
     return grid 
 class Game: 
-    """classe permettant d'initialiser la partie et de débuter un historique des coups joués """
-    def __init__(self):
-        self.moves : list[str]  = []
-        self.turn : int =0 
+    """classe permettant d'initialiser la partie et de débuter un historique des coups joués
+     les paramètre à prendre en compte:
+     -le nom des joueurs (player1 et player2)
+     -le timer de base (en seconde)
+       """
+    def __init__(self,player1 :str, player2 :str, timer : int):
+    
+        self.moves : list[str]  = [] #historique de la partie
+        self.turn : int =0 # initialisation du tour de jeu 
         self.grid=init_grid()
+
+
+        players : list[str] = random.shuffle([player1,player2])#tirage pour savoir qui va être le joueur blanc et qui va être le joueur noir
+        self.white_player=[players[0], timer]
+        self.black_player=[players[1], timer]
