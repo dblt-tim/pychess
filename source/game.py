1c43199acdfb8cf4965598695531f5cff8107b16
dblt-tim <timeodiebolt@gmail.com>
2025-03-01 18:19:57 +0100

    avancement de la fonction move

--- source/game.py
+++ source/game.py
@@ -34,7 +34,7 @@ class Game:
         
         # on définit aléatoirement quel joueur commence (en jouant donc les blancs)
         self.wplayer, self.bplayer = random.shuffle([player1, player2])
-
+        self.turn = 0 # 0 pour les blancs et 1 pour les noirs
     def move(self, from_pos, to_pos):
         """
         1. on vérifie si le roi n'est pas en échec
@@ -52,4 +52,29 @@ class Game:
         3. le roi ne doit pas être en échec
         4. le roi ne doit pas arriver en position d'échec
         """
+        player = "w" if not self.turn else "b"
+        opp = "b" if not self.turn else "w"
+
+        # on vérifie tous les coups théoriquement possibles par les deux camps
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                pawn = grid[i][j][1] if grid[i][j] != "" else None
+                if pawn == "R":
+                    pass # parse rook moves
+                if pawn == "N":
+                    pass # parse knight moves
+                if pawn == "B":
+                    pass # parse bishop moves
+                if pawn == "Q":
+                    pass # parse queen moves
+                if pawn == "K":
+                    pass # parse king moves
+                if pawn == "P":
+                    pass # parse pawn moves (hardest)
+        if k in opp_possible_moves:
+            pass # quand le roi est en échec, vérifier ensuite si il n'y a pas de solution pour le protéger
+        else:
+            pass # si le roi n'est pas en échec on vérifie tout de même si le mouvement demandé ne le met pas en échec
+                 # puis on fait le move
+
         
